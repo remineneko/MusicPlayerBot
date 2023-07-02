6fb6c65c2d58a3f0c69aec314cd66d3e2f03c534
remineneko <henry11102000@gmail.com>
2023-07-03 04:28:16 +0700

    updated code in download_mediametadata.py

--- src/player/youtube/download_media.py
+++ src/player/youtube/download_media.py
@@ -2,10 +2,11 @@ from yt_dlp import YoutubeDL
 from typing import List
 import os
 import asyncio
+import librosa
 
 from constants import MUSIC_STORAGE
 from src.player.media_metadata import MediaMetadata
-from src.player.alter_title import alter_title
+from src.player.timestamp_parser import parse_timestamp
 
 
 class NoVideoInQueueError(Exception):
@@ -24,6 +25,7 @@ class SingleDownloader:
     def download(self):
         if self._data is not None and not isExist(self._data):
             self._ydl.download([self._data.original_url])
+            self._data.duration = int(librosa.get_duration(path=toPath(self._data)))
 
 
 class Downloader:
@@ -51,9 +53,29 @@ class Downloader:
             for sin_ind in range(1, len(self._data)):
                 SingleDownloader(self._data[sin_ind], self._ydl).download()
 
-
 def isExist(info_dict: MediaMetadata) -> bool:
-    try:
-        return os.path.isfile(os.path.join(MUSIC_STORAGE,'{}.mp3'.format(alter_title(info_dict.id))))
-    except AttributeError:
-        return False
+    return get_path(info_dict)[0]
+
+def get_path(info_dict: MediaMetadata):
+    if hasattr(info_dict, 'id'):
+        id_path = os.path.join(MUSIC_STORAGE, f'{info_dict.id}.{info_dict.ext}')
+        name_path_3 = os.path.join(MUSIC_STORAGE, f'{info_dict.title} [{info_dict.id}].{info_dict.ext}')
+    else:
+        id_path = None
+        name_path_3 = None
+    
+    name_path_1 = os.path.join(MUSIC_STORAGE, f'{info_dict.title}.{info_dict.ext}')
+    name_path_2 = os.path.join(MUSIC_STORAGE, f'{info_dict.title} [{info_dict.title}].{info_dict.ext}')
+    
+    paths = [name_path_1, name_path_2]
+    if id_path and name_path_3:
+        paths.extend([id_path, name_path_3])
+
+    for path in paths:
+        if os.path.isfile(path):
+            return True, path
+
+    return False, None
+
+def toPath(info_dict: MediaMetadata):
+    return get_path(info_dict)[1]
