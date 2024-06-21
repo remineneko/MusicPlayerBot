f20a8608978c0ff42e9884537f18ba755f585467
remineneko <56041330+remineneko@users.noreply.github.com>
2024-06-21 10:46:47 +0700

    added arxiv module

---
+++ src/cogs/arxiv.py
@@ -0,0 +1,113 @@
+from discord.ext import commands, tasks
+import discord
+
+from arxiv import Client, Search, SortCriterion, Result
+from collections import defaultdict
+from datetime import datetime, timezone, timedelta
+from typing import Dict, List
+
+
+class arXivGuildSession:
+    def __init__(self):
+        self.client: Client = Client()
+        self.prev_subs_search_res: List[Result] = list()
+
+
+class arXiv(commands.Cog):
+    def __init__(
+        self, 
+        bot: discord.ext.commands.Bot
+    ):
+        """
+        Initialize the bot.
+
+        Args:
+            bot (discord.ext.commands.Bot): The Discord Bot client.
+        """
+        self._bot = bot
+        self._guild_sessions: Dict[int, arXivGuildSession] = defaultdict(lambda: arXivGuildSession())
+
+    def _find_guild_sesh(self, ctx: commands.Context):
+        return self._guild_sessions[ctx.guild.id]
+
+    @commands.hybrid_command(name='subscribe_paper')
+    async def subscribe_paper(
+        self,
+        ctx: commands.Context,
+        *,
+        query: str
+    ):
+        await ctx.send("Subscribed! The bot will find new papers every hour under the provided query.")
+        self._subscribe.start(ctx, query)
+
+    @tasks.loop(hours=1)
+    async def _subscribe(self, ctx: commands.Context, query: str):
+        searcher = Search(
+            query=query,
+            max_results=100, # theres no way there will be more than 100 papers PER HOUR if anyone knows what they are looking for...
+            sort_by=SortCriterion.SubmittedDate
+        )
+
+        results = list(self._find_guild_sesh(ctx).client.results(searcher))
+
+        time_now = datetime.now(tz=timezone.utc)
+        time_before = time_now - timedelta(hours=1)
+
+        # filtering results
+        results = [r for r in results if r.published > time_before and r.published < time_now]
+        if len(list(results)) > 0:
+            for result in results:
+                embed = self._build_embed(result)
+                await ctx.send(embed=embed)
+
+    @commands.hybrid_command()
+    async def findpaper(self, ctx: commands.Context, *, query: str):
+        searcher = Search(
+            query=query,
+            max_results=5, 
+            sort_by=SortCriterion.SubmittedDate
+        )
+
+        results = list(self._find_guild_sesh(ctx).client.results(searcher))
+
+        if len(results) > 0:
+            for result in results:
+                embed = self._build_embed(result)
+                await ctx.send(embed=embed)
+        else:
+            await ctx.send("No results have been found.")
+
+    def _build_embed(self, result: Result):
+        result_embed = discord.Embed(
+            title=result.title,
+            description=", ".join([a.name for a in result.authors]),
+            color=discord.Color.dark_red(),
+            url=result.pdf_url
+        )
+
+        result_embed.set_thumbnail(url='https://public.boxcloud.com/api/2.0/internal_files/804104772302/versions/860288648702/representations/png_paged_2048x2048/content/1.png?access_token=1!AEgx010olcDzcZEirc7owzQmEvWa84G6rA90m8ORJjnd0eqKllWt5-ymHnXR-qRhlHZnIT4Xj5oWfq2tWF9jrE2Xc08pU-RincsPsT3e-zXnBI7V-MJJzJVM3i-L5nB-QM2CT9aTTbPmv0orRSOFgqgLkzgzxrSamLFNYyq3vsewFD633tYGcBK0Q-WB47UuYDXDZsYRNwsga7gkDgNdSqSD7z2-nyPPerBH0jELUVRfzEF978DfG59CyM1c2ejIXik_gFXko4o9r_O50gMCo0OiaV5I3irRUnbOFRVrEhFGyr50YZX3x2zjbCl4cIabmn5DIsh6uoKSN8MX-zI6KzlBbLrrnZp724atUaSGwpGeKyutE2q3WhYdeBtibBvQ_9jSvNefbpRTwQkN1fGG-Jtnc28_av3QHtjXcblwTMh1miaqhLqZOSCvoCeZDX4kh8-hixD3AZhxL8nLgvpF5_fSgTXo4arzZvnpkkt69A-mwMR6x9Xgt6HxC5O_dziu3WeLzcLgFlISOWy3OuEuykh5ndJvvLXjet0_6XBXCNZtFqqQT8sAalxTJ-6_szxaYUgC40ntMqLhi_m5qUabNgWYGhlHHzOhkfmqfQ..&shared_link=https%3A%2F%2Fcornell.app.box.com%2Fv%2Farxiv-logomark-small-png&box_client_name=box-content-preview&box_client_version=2.108.0')
+
+        abs_splitted = result.summary.split()
+        end_abs = []
+
+        for abs_part in abs_splitted:
+            if sum([len(s) for s in end_abs]) + len(abs_part) < 300:
+                end_abs.append(abs_part)
+
+            else:
+                break
+
+        abstract = f"{' '.join(end_abs)}... [More]({result.pdf_url.replace('pdf', 'abs')})"
+  
+        result_embed.add_field(
+            name='Abstract',
+            value=abstract
+        )
+
+        result_embed.set_footer(text='Thank you to arXiv for use of its open access interoperability.')
+
+        return result_embed
+        
+
+async def setup(bot):
+    await bot.add_cog(arXiv(bot))
